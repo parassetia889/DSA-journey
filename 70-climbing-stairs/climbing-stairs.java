800276c0621267839ff60a2a977e2914f755910d
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2024-05-15 09:53:27 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (73.55%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,17 +1,17 @@
 class Solution {
     public int climbStairs(int n) {
        
-       if(n <= 3)
+       if(n <= 2)
         return n;
     
-        int prev1 = 3;
-        int prev2 = 2;
+        int first = 1;
+        int second = 2;
         int curr = 0;
 
-        for(int i = 3; i< n; i++){
-            curr = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = curr;
+        for(int i = 3; i <= n; i++){
+            curr = first + second;
+            first = second;
+            second = curr;
             }
 
         return curr;
