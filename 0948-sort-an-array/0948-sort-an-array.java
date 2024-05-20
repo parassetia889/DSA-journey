94bfbcf6fa37a4d062e64edfa5f5b2fa5ca09881
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2024-05-21 00:35:45 +0530

    Time: 26 ms (67.61%), Space: 56.4 MB (37.31%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,21 +1,56 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        
-        int n = nums.length;
-        for(int i = 1; i < n; i++){
-
-            int temp = nums[i];
-            int j = i-1;
-            for(; j >= 0; j--){
-
-                if(nums[j] > temp)
-                    nums[j+1] = nums[j];
-                else
-                    break;
-            }
-            nums[j+1] = temp;
-        }
+        mergeSort(nums, 0, nums.length-1);
 
         return nums;
+    }
+
+    public static void mergeSort(int[] nums, int s, int e){
+
+        if(s >= e)
+            return;
+
+        int mid = (e-s)/2 + s;
+
+        //sort left part
+        mergeSort(nums, s, mid);
+
+        //sort right part
+        mergeSort(nums, mid+1, e);
+
+        merge(nums, s, e);
+    }
+
+    public static void merge(int[] nums, int s, int e){
+
+        int mid = (e+s)/2;
+        int l1 = mid-s+1;
+        int l2 = e-mid;
+
+        int[] first = new int[l1];
+        int[] second = new int[l2];
+
+        for(int i = 0; i < l1; i++)
+            first[i] = nums[s+i];
+
+        for(int i = 0 ; i < l2; i++)
+            second[i] = nums[mid+1+i];
+
+        //merging
+        int ind1 = 0, ind2 = 0;
+        int mainIndx = s;
+
+        while(ind1 < l1 && ind2 < l2){
+            if(first[ind1] < second[ind2])
+                nums[mainIndx++] = first[ind1++];
+            else
+                nums[mainIndx++] = second[ind2++];
+        }
+
+        while(ind1 < l1)
+            nums[mainIndx++] = first[ind1++];
+
+        while(ind2 < l2)
+            nums[mainIndx++] = second[ind2++];
     }
 }
