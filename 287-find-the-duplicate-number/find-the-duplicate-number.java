574965002fd59417540cc774a598f1fe24412cc7
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2024-05-29 22:51:31 +0530

    Time: 3 ms (90.74%) | Memory: 58 MB (58.13%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        
+
+        for(int i = 0 ; i < nums.length; i++){
+            int ind = Math.abs(nums[i])-1;
+
+            if(nums[ind] < 0)
+                return ind+1;
+
+            nums[ind] *= -1;
+        }
+
+        return -1;
+    }
+}
