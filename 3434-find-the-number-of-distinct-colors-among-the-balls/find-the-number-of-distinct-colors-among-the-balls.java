57d1036f9ede3bd153b064e9fe03d05966f71347
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2024-05-30 22:42:39 +0530

    Time: 49 ms (42.59%) | Memory: 99.3 MB (26.81%) - LeetSync

--- 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.java
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.java
@@ -1,29 +1,28 @@
 class Solution {
     public int[] queryResults(int limit, int[][] queries) {
         
-        Map<Integer, Integer> ballToColors = new HashMap<>();
+        Map<Integer, Integer> ballToColor = new HashMap<>();
         Map<Integer, Integer> colorCount = new HashMap<>();
 
         int ans[] = new int[queries.length];
-
         int i = 0;
         for(int[] query : queries){
             int ball = query[0];
             int color = query[1];
 
-            if(ballToColors.containsKey(ball)){
+            if(ballToColor.containsKey(ball)){
 
-                int existingColor = ballToColors.get(ball);
+                int existingColor = ballToColor.get(ball);
                 colorCount.put(existingColor, colorCount.get(existingColor)-1);
                 if(colorCount.get(existingColor) == 0)
                     colorCount.remove(existingColor);
             }
-            ballToColors.put(ball, color);
-            colorCount.put(color, colorCount.getOrDefault(color,0)+1);
+
+            ballToColor.put(ball, color);
+            colorCount.put(color, colorCount.getOrDefault(color, 0)+1);
 
             ans[i++] = colorCount.size();
         }
-
         return ans;
     }
 }
