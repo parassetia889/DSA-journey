9ef3daf06231fee0316e403ad0b72aeffeae03df
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2024-06-09 16:41:26 +0530

    Time: 3 ms (35.56%) | Memory: 43.8 MB (8.70%) - LeetSync

---
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkIfExist(int[] nums) {
+        
+        int n = nums.length;
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < n; i++)
+            map.put(nums[i], i);
+
+        for(int i = 0; i < n; i++){
+            if(map.containsKey(2*nums[i]) && i != map.get(2*nums[i]))
+                return true;
+        }
+
+        return false;
+    }
+}
