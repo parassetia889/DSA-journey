e09dac17a63019fbbad85df68d0aad8d07e1d366
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2024-06-12 00:42:00 +0530

    Time: 4 ms (23.80%) | Memory: 42.1 MB (45.23%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.java
+++ 1217-relative-sort-array/relative-sort-array.java
@@ -1,34 +1,31 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
         
-        int n1 = arr1.length;
-        int n2 = arr2.length;
+    Map<Integer, Integer> countMap = new HashMap<>();
 
-        List<Integer> res = new ArrayList<>();
-        List<Integer> remaining = new ArrayList<>();
+    for(int i : arr2)
+        countMap.put(i, 0);
 
-        HashMap<Integer, Integer> map = new HashMap<>();
+    List<Integer> res = new ArrayList<>();
+    List<Integer> remaining = new ArrayList<>();
 
-        for(int i : arr2)
-            map.put(i,0);
+    for(int val : arr1){
+        if(countMap.containsKey(val))
+            countMap.put(val, countMap.get(val)+1);
+        else
+            remaining.add(val);
+    }
 
-        for(int val : arr1){
-            if(map.containsKey(val))
-                map.put(val, map.get(val)+1);
-            else
-                remaining.add(val);
-        }
+    Collections.sort(remaining);
 
-        Collections.sort(remaining);
+    for(int val : arr2){
+        for(int j = 0; j < countMap.get(val); j++)
+            res.add(val);
+     }
 
-        for(int val : arr2){
-            for(int j = 0; j < map.get(val); j++)
-                res.add(val);
-        }
 
-        res.addAll(remaining);
+     res.addAll(remaining);
 
-        return res.stream().mapToInt(Integer::intValue).toArray();
-
+     return res.stream().mapToInt(Integer::intValue).toArray();
     }
 }
