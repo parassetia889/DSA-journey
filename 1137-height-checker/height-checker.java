573b3d37ad50354abd2e8b48efa1caecbda1e7e2
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2024-06-14 16:55:18 +0530

    Time: 3 ms (8.57%) | Memory: 41.4 MB (26.65%) - LeetSync

--- 1137-height-checker/height-checker.java
+++ 1137-height-checker/height-checker.java
@@ -1,19 +1,31 @@
 class Solution {
+
+    public void sort(int nums[]){
+        int n = nums.length;
+
+        for(int i = 0; i < n-1; i++){
+            boolean swap = false;
+            for(int j = 0; j < n-i-1; j++){
+                if(nums[j] > nums[j+1]){
+                    swap = true;
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] =temp;
+                }
+            }
+            if( !swap)
+                break;
+        }
+    }
+
     public int heightChecker(int[] heights) {
         
-        int n = heights.length;
-        int[] expected = new int[n];
-
-        int j = 0;
-        for(int i : heights){
-            expected[j] = i;
-            j++;
-        }
-
-        Arrays.sort(expected);
-        
         int count = 0;
-        for(int i = 0; i < expected.length; i++){
+        int n = heights.length;
+        int[] expected = heights.clone();
+        sort(expected);
+
+        for(int i = 0; i < n; i++){
             if(expected[i] != heights[i])
                 count++;
         }
