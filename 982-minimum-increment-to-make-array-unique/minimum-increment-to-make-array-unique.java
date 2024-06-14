b2a20fb5ab35c3308b298309a298c90631c52ded
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2024-06-14 13:55:23 +0530

    Time: 563 ms (5.57%) | Memory: 60 MB (51.48%) - LeetSync

---
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minIncrementForUnique(int[] nums) {
+
+     int minMoves = 0;
+     Arrays.sort(nums);
+
+     for(int i = 1; i < nums.length; i++){
+        if(nums[i] <= nums[i-1])
+            while(nums[i] <= nums[i-1]){
+                nums[i] += 1;
+                minMoves++;
+            }
+        
+     }   
+
+     return minMoves;
+    }
+}
