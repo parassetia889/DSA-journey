d323a57c87b4f976fa21586ef8ff4ba7df7be414
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2024-06-14 14:40:15 +0530

    Time: 37 ms (72.13%), Space: 60.2 MB (38.03%) - LeetHub

--- 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.java
+++ 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.java
@@ -5,10 +5,11 @@ class Solution {
        Arrays.sort(nums);
 
        for(int i = 1; i < nums.length; i++){
-        if(nums[i] <= nums[i-1])
-            while(nums[i] <= nums[i-1]){
-                nums[i] += 1;
-                minMoves++;
+        if(nums[i] <= nums[i-1]){
+            int moves = nums[i-1]-nums[i]+1;
+            minMoves +=  moves;
+
+            nums[i] = nums[i-1]+1;
             }
        } 
 
