b1cada512f28c30b4e5c738854deb7f0c25afd7f
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2024-07-14 10:14:42 +0530

    Time: 1 ms (100%), Space: 46.2 MB (15.4%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -3,41 +3,55 @@ class Solution {
 
         int m = nums1.length;
         int n = nums2.length;
+        int len = m+n;
 
-        int newArr[] = new int[m+n];
-        int i = 0, j = 0, k = 0;
+        int indx2 = len/2, indx1 = len/2-1;
+        int ind1ele = -1, ind2ele = -1;
+
+        int cnt = 0;
+        int i = 0,  j =  0, k = 0;
         double median = 0;
 
         while(i < m && j < n){
             if(nums1[i] < nums2[j]){
-                newArr[k] = nums1[i];
-                i++; k++;
+                if(cnt == indx1)
+                    ind1ele = nums1[i];
+                if(cnt == indx2)
+                    ind2ele = nums1[i];
+                i++; cnt++;
             }
             else{
-                newArr[k] = nums2[j];
-                j++; k++;
+                if(cnt == indx1)
+                    ind1ele = nums2[j];
+                if(cnt == indx2)
+                    ind2ele = nums2[j];
+                j++; cnt++;
             }
+
+            // if(ind1ele != -1 && ind2ele != -1)
+            //     break;
         }   
 
-        while(i < m && k < newArr.length){
-            newArr[k] = nums1[i];
-            i++; k++;
+        while(i < m ){
+           if(cnt == indx1)
+                ind1ele = nums1[i];
+            if(cnt == indx2)
+                ind2ele = nums1[i];
+            i++; cnt++;
         }
 
-        while(j < n && k < newArr.length){
-            newArr[k] = nums2[j];
-            j++; k++;
+        while(j < n){
+           if(cnt == indx1)
+                ind1ele = nums2[j];
+            if(cnt == indx2)
+                ind2ele = nums2[j];
+            j++; cnt++;
         }
 
-        if(newArr.length%2 != 0)
-            median = newArr[newArr.length/2];
-        else{
-            int val1 = newArr[newArr.length/2];
-            int val2 = newArr[(newArr.length/2) - 1];
-            median = (val1+val2)/2.0;
-
-            // median = (double)(newArr[(newArr.length/2) - 1] + newArr[newArr.length/2])/2;
-        }
+        if(len%2 != 0)
+            median = ind2ele;
+        else
+            median = (ind1ele+ind2ele)/2.0;
 
         return median;
     }
