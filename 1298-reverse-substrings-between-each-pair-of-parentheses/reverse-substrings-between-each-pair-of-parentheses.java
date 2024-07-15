aba07cbd6e0379e0a83e6651a2e3d675d983bfe9
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2024-07-15 08:21:21 +0530

    Time: 1 ms (88.87%) | Memory: 41.5 MB (71.43%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
@@ -1,29 +1,32 @@
 class Solution {
     public String reverseParentheses(String s) {
         
-        StringBuilder res = new StringBuilder();
-        Stack<Integer> st = new Stack<>();
+        int n = s.length();
+        Stack<Integer> stack = new Stack<>();
+        int[] pair = new int[n];
 
-        for(char ch : s.toCharArray()){
-            if(ch == '(')
-                st.push(res.length());
-            else if(ch == ')'){
-                int startIndx = st.pop();
-                reverse(res, startIndx, res.length()-1);
+        for(int i = 0; i < n; i++){
+            char ch = s.charAt(i);
+            if(ch =='(')
+                stack.push(i);
+            else if(ch ==')'){
+                int j = stack.pop();
+                pair[i] = j;
+                pair[j] = i;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int currIndx = 0, dir = 1; currIndx < n; currIndx += dir){
+            
+            if(s.charAt(currIndx) == '(' || s.charAt(currIndx) == ')'){
+                dir *= -1;
+                currIndx = pair[currIndx];
             }
             else
-                res.append(ch);
+                sb.append(s.charAt(currIndx));
         }
-        return res.toString();
-    }
 
-    private static void reverse(StringBuilder sb, int start, int end){
-
-        while(start < end){
-            char temp = sb.charAt(start);
-            sb.setCharAt(start, sb.charAt(end));
-            sb.setCharAt(end, temp);
-            start++; end--;
-        }
+        return sb.toString();
     }
 }
