4564e516beaa258ae51390d2d86e56c47b6d0a22
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2024-08-02 22:59:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (22.90%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -14,14 +14,22 @@ class Solution {
         if(head == null || head.next == null)
             return head;
 
-        ListNode prev = null, curr = head;
-
-        while(curr != null){
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
+        ListNode newHead = reverseList(head.next);
+        
+        // Save a reference to the node following
+        // the current 'head' node.
+        ListNode front = head.next;
+        
+        // Make the 'front' node point to the current
+        // 'head' node in the reversed order.
+        front.next = head;
+        
+        // Break the link from the current 'head' node
+        // to the 'front' node to avoid cycles.
+        head.next = null;
+        
+        // Return the 'newHead,' which is the new
+        // head of the reversed linked list.
+        return newHead;
     }
 }
