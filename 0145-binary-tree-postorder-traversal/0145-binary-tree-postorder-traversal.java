cbbf8c2c282ed66e400367638ad31464d09041c8
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2024-08-25 21:35:44 +0530

    Time: 0 ms (100%), Space: 41.3 MB (82.09%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -15,18 +15,20 @@
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        return traversal(root, new ArrayList<Integer>());
+
+        List<Integer> res = new ArrayList<Integer>();
+        traversal(root, res);
+        return res;
     }
 
-    private List<Integer> traversal(TreeNode root, List<Integer> res){
+    private void traversal(TreeNode root, List<Integer> res){
         if(root == null)
-            return res;
+            return;
         
         
         traversal(root.left, res);
         traversal(root.right, res);
 
         res.add(root.val);
-        return res;
     }
 }
