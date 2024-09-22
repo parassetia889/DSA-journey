25f36d5ff8c2409a90303b9caa6e62f19393232e
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2024-09-22 18:05:59 +0530

    Time: 8 ms (100%), Space: 45.4 MB (50%) - LeetHub

--- 3496-minimum-number-of-seconds-to-make-mountain-height-zero/3496-minimum-number-of-seconds-to-make-mountain-height-zero.java
+++ 3496-minimum-number-of-seconds-to-make-mountain-height-zero/3496-minimum-number-of-seconds-to-make-mountain-height-zero.java
@@ -1,28 +1,29 @@
 class Solution {
     public long minNumberOfSeconds(int mountainHeight, int[] workerTimes) {
-        long left = 1, right = (long) 1e18; 
         
-        while (left < right) {
-            long mid = left + (right - left) / 2;
-            if (canFinish(mountainHeight, workerTimes, mid)) {
+        long left = 0, right = (long) 1e18;
+
+        while(left < right){
+            long mid = left + (right-left)/2;
+
+            if(canFinish(mountainHeight , workerTimes, mid))
                 right = mid;
-            } else {
-                left = mid + 1;
-            }
+            else
+                left = mid+1;
         }
         return left;
     }
 
-    private boolean canFinish(int mountainHeight, int[] workerTimes, long maxTime) {
-        long totalReducedHeight = 0;
+    private boolean canFinish(int mountainHeight, int[] workerTimes, long maxTime){
+        long reduced = 0;
 
-        for (int time : workerTimes) {
-            long maxHeight = (long) (-1 + Math.sqrt(1 + 8 * maxTime / time)) / 2;
-            totalReducedHeight += maxHeight;
-            if (totalReducedHeight >= mountainHeight) {
+        for(int time : workerTimes){
+            long maxH = (long)(-1 +  Math.sqrt(1+8 * maxTime/time))/2;
+
+            reduced += maxH;
+            if(reduced >= mountainHeight)
                 return true;
-            }
         }
-        return totalReducedHeight >= mountainHeight;
+        return reduced >= mountainHeight;
     }
 }
