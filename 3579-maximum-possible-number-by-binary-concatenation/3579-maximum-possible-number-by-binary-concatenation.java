8fbfbe1b867d48098cbd4f0e44fc7c1f7020aec5
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2024-10-06 15:03:52 +0530

    Time: 4 ms (100%), Space: 43.2 MB (100%) - LeetHub

--- 3579-maximum-possible-number-by-binary-concatenation/3579-maximum-possible-number-by-binary-concatenation.java
+++ 3579-maximum-possible-number-by-binary-concatenation/3579-maximum-possible-number-by-binary-concatenation.java
@@ -1,44 +1,44 @@
 class Solution {
 
-    public static String toBinary(int num){
+    public static String toBinaryString(int num){
         return Integer.toBinaryString(num);
     }
+
     public int maxGoodNumber(int[] nums) {
         
-        List<int[]> permutes = new ArrayList<>();
-        generatePermutations(nums, 0, permutes);
+        List<int[]> permutations = new ArrayList<>();
+        generatePermutations(nums, 0, permutations);
 
         int max = 0;
-
-        for(int[] perm : permutes){
+        for(int[] permute : permutations){
             StringBuilder sb = new StringBuilder();
 
-            for(int num : perm)
-                sb.append(toBinary(num));
+            for(int num : permute){
+                sb.append(toBinaryString(num));
+            }
 
-        int concatValue = Integer.parseInt(sb.toString(),2);
+            int concatenatedValue = Integer.parseInt(sb.toString(),2);
 
-        max = Math.max(max, concatValue);
+            max = Math.max(max, concatenatedValue);
         }
-
         return max;
+
     }
 
     public static void generatePermutations(int[] nums, int start, List<int[]> res){
         if(start == nums.length){
             res.add(nums.clone());
-            return;
+            return ;
         }
 
-        for(int i = 0; i < nums.length; i++){
+        for(int i = start; i< nums.length; i++){
             swap(nums, start, i);
             generatePermutations(nums, start+1, res);
             swap(nums, start, i);
         }
-        
     }
 
-    private static void swap(int[] nums, int i, int j){
+    public static void swap(int[] nums, int i, int j){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
