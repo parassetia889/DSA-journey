d0f2c4bea929673a24fab4d5af25ea92376e7b51
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2024-10-06 15:24:34 +0530

    Time: 798 ms (8.02%) | Memory: 45.1 MB (13.79%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -2,14 +2,16 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         
         s1 = sort(s1);
-        for(int i = 0 ; i <= s2.length()-s1.length(); i++){
+        for(int i = 0; i <= s2.length()-s1.length(); i++){
             if(s1.equals(sort(s2.substring(i, i+s1.length()))))
                 return true;
         }
+
         return false;
     }
 
-    public String sort(String str){
+    public static String sort(String str){
+        
         char[] chars = str.toCharArray();
         Arrays.sort(chars);
         return new String(chars);
