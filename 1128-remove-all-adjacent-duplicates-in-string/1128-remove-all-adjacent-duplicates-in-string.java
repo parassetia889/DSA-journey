674b46f18c9d46a310c8f50f64412a6cd6173242
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2024-12-14 09:48:29 +0530

    Time: 36 ms (41.58%), Space: 45.8 MB (18.15%) - LeetHub

---
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        
+        Stack<Character> st = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(!st.isEmpty() && st.peek() == ch)
+                st.pop();
+            else
+                st.push(ch);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while( !st.isEmpty()){
+            sb.append(st.pop());
+        }
+
+        return sb.reverse().toString();
+    }
+}
