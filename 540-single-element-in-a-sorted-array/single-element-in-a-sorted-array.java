daaa7d7fae599ed281699162f6b73d78b13832b8
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2024-12-15 12:24:33 +0530

    Time: 0 ms (100.00%) | Memory: 50.6 MB (19.29%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -12,18 +12,20 @@ class Solution {
             return nums[n-1];
 
         int low = 1, high = n-2;
-
         while(low <= high){
-            int mid = (low + high)/2;
+            
+            int mid = (low+high)/2;
 
-            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1])
+            if(nums[mid] != nums[mid-1] & nums[mid] != nums[mid+1])
                 return nums[mid];
 
-            if((nums[mid] == nums[mid-1] && mid%2 != 0) || (nums[mid] == nums[mid+1] && mid%2 == 0))
+            if((nums[mid] == nums[mid-1] && mid%2 != 0)  || (nums[mid] == nums[mid+1] && mid%2 == 0)){
                 low = mid+1;
-            else
+
+            }else{
                 high = mid-1;
-        } 
+            }
+        }
         return -1;
     }
 }
