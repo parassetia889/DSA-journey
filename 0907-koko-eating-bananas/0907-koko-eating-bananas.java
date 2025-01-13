8c31e9a388fd5c8f0b893aa09908fa4bee2cd2d4
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2025-01-13 19:10:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,34 +1,36 @@
 class Solution {
-
-    public static int getHours(int rate, int[] piles){
-        int hrs = 0;
-
+    public int minEatingSpeed(int[] piles, int h) {
+        
+        int low = 1, high = -1;
+        int sum = 0;
         for(int pile : piles){
-            hrs += Math.ceil((double)pile/rate);
+            high = Math.max(high, pile);
+            
         }
 
-        return hrs;
-    }
-    public int minEatingSpeed(int[] piles, int h) {
-        int max = Integer.MIN_VALUE;
-
-        for(int i : piles)
-            max = Math.max(i, max);
-
-        int low = 1, high = max, ans = -1;
-
+        high = sum;
+        int ans = low;
         while(low <= high){
-            int mid = (low+high)/2;
 
-            int hrs = getHours(mid, piles);
+            int mid = (low+high)/2;
+            int hrs = calculateDays(mid, piles);
+
             if(hrs <= h){
                 ans = mid;
                 high = mid-1;
-                }
-            else
+            }else{
                 low = mid+1;
-
+            }
         }
         return ans;
+    }
+
+    public int calculateDays(int hourly, int[] piles){
+        int totalH = 0;
+
+        for(int pile : piles){
+            totalH += Math.ceil((double)pile/hourly);
+        }
+        return totalH;
     }
 }
