61cef43801634f5a8150b52de5c59ff2460d163f
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2025-10-31 17:45:21 +0530

    Time: 1590 ms (5%), Space: 56 MB (5.19%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,35 +1,26 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        
-        Arrays.sort(nums);
-        int n = nums.length;
-        List<List<Integer>> res = new ArrayList<>();
+        Set<List<Integer>> res = new HashSet<>();
 
-        for(int i = 0; i < n; i++){
-        
-            if(i != 0 && nums[i] == nums[i-1])
-                continue;
-
-            int j = i+1, k = n-1;
-            while(j < k){
-                int sum = nums[i]+nums[j]+nums[k];
-                
-                if(sum == 0){
-                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-                    k--;
+        for(int i = 0; i < nums.length; i++){
+            Set<Integer> set = new HashSet<>();
+            
+            for(int j = i+1; j < nums.length; j++){
+                int third = -(nums[i]+nums[j]);
+                if(set.contains(third)){
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(third);
                     
-                    while(j<k && nums[j] == nums[j-1]) j++;
-                    while(j < k && nums[k] == nums[k+1]) k--;
-                }
-                else if(sum < 0)
-                    j++;
-                else 
-                    k--;
+                    Collections.sort(temp);
+                    res.add(temp);
 
-                
+                }
+                set.add(nums[j]);
             }
         }
-        return res;
+
+        return new ArrayList<>(res);
     }
 }
