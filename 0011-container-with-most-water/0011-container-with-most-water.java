ca71fd51971689691dfa9ede476266ea48560854
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2025-11-04 13:53:19 +0530

    Time: 5 ms (77.76%), Space: 77.5 MB (6.32%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -8,7 +8,7 @@ class Solution {
             int water = Math.min(height[l], height[r]) *  (r-l);
             max = Math.max(max, water);
 
-            if(l < r)
+            if(height[l] < height[r])
                 l++;
             else
                 r--;
