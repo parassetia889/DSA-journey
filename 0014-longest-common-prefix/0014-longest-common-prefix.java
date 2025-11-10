0d46c191630ab73650ac476823fe9810c976e1cb
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2025-11-10 16:00:21 +0530

    Time: 1 ms (58.06%), Space: 43.2 MB (7.76%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -3,18 +3,21 @@ class Solution {
         
         if(strs == null || strs.length == 0)
             return "";
+        
+        if(strs.length == 1)
+            return strs[0];
 
-        String prefix = strs[0];
 
-        for(int i = 1; i < strs.length; i++){
+        Arrays.sort(strs);
+        int n = strs.length;
+        int N = Math.min(strs[0].length(), strs[n-1].length());
 
-            while(strs[i].indexOf(prefix) != 0){
-                prefix = prefix.substring(0, prefix.length()-1);
-
-                if(prefix.isEmpty())
-                    return "";
+        for(int i = 0 ; i < N; i++){
+            if(strs[0].charAt(i) != strs[n-1].charAt(i)){
+                return strs[0].substring(0,i);
             }
         }
-        return prefix;
+
+        return strs[0];
     }
 }
