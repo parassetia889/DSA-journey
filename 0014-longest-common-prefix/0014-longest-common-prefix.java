de0c548f36b6af012096dc0287b141a9ff4535e0
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2025-11-10 15:44:54 +0530

    Time: 0 ms (100%), Space: 43.4 MB (5.81%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -8,13 +8,13 @@ class Solution {
 
         for(int i = 1; i < strs.length; i++){
 
-            while(strs[i].indexOf(prefix) != 0)
+            while(strs[i].indexOf(prefix) != 0){
                 prefix = prefix.substring(0, prefix.length()-1);
 
                 if(prefix.isEmpty())
                     return "";
+            }
         }
-
         return prefix;
     }
 }
