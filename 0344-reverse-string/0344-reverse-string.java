8c1204bc880637d84b5678f318bfb4c49858d7e6
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2025-11-10 16:06:25 +0530

    Time: 0 ms (100%), Space: 48.2 MB (76.08%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,21 +1,13 @@
 class Solution {
     public void reverseString(char[] s) {
-        
-        int start = 0, end = s.length-1;
+        int l = 0, r = s.length-1;
 
-        reverse(s, 0, s.length-1);
-    }
-
-    public void reverse(char[] s, int left, int right){
-        if( left >= right)
-            return;
-
-            char temp = s[left];
-            s[left] = s[right];
-            s[right] = temp;
-
-        reverse(s, left+1, right-1);
-          
+        while(l < r){
+            char temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
+            l++; r--;
+        }
 
     }
 }
