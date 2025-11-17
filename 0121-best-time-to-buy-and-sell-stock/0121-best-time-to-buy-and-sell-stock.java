839a1652481263f82fc16f03e9e8faeb0f8fa168
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2025-11-17 15:51:25 +0530

    Time: 1 ms (99.87%), Space: 93.9 MB (21.79%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,13 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
         
-        int profit = 0, cp = prices[0];
-        for(int i = 1; i < prices.length; i++){
-            if(prices[i] < cp)
-                cp = prices[i];
-            else{
-                profit = Math.max(profit, prices[i]-cp);
-            }
+        int cp = prices[0], profit = 0;
+
+        for(int indx=0; indx < prices.length; indx++){
+            if(prices[indx] < cp)
+                cp = prices[indx];
+            
+            else
+                profit = Math.max(profit, prices[indx]-cp);
         }
 
         return profit;
