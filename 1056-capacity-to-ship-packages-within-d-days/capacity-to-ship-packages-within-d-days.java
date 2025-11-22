2f4f7d070019f3eba95f86f142c0204a85a599a8
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2025-11-22 15:55:00 +0530

    Time: 11 ms (75.17%) | Memory: 50.1 MB (21.89%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,42 +1,40 @@
 class Solution {
-
-    public static boolean checkWeight(int[] weights, int maxWeight, int days) {
-        int daysRequired = 1;
-        int currentWeight = 0;
-
-        for (int weight : weights) {
-            if (currentWeight + weight <= maxWeight) {
-                currentWeight += weight;
-            } else {
-                daysRequired++;
-                currentWeight = weight;
-                if (daysRequired > days) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
     public int shipWithinDays(int[] weights, int days) {
-        int low = 0, high = 0;
-
-        for (int weight : weights) {
-            low = Math.max(low, weight);
-            high += weight;
+        
+        int maxVal = weights[0], sum=0;
+        for(int weight : weights){
+            sum += weight;
+            maxVal = Math.max(maxVal, weight);
         }
 
-        int ans = high;
-        while (low <= high) {
-            int mid = (low + high) / 2;
+        int low = maxVal, high = sum;
 
-            if (checkWeight(weights, mid, days)) {
+        int ans = low;
+        while(low <= high){
+            int mid = (low+high)/2;
+
+            int totalDays = getDays(weights, mid);
+            if(totalDays <= days){
                 ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+                high = mid-1;
+            }else{
+                low = mid+1;
             }
         }
         return ans;
+    }
+
+    public int getDays(int[] weights, int capacity){
+        int totalDays = 1, currCapacity=0;
+
+        for(int weight : weights){
+            if(currCapacity+weight > capacity){
+                totalDays += 1;
+                currCapacity = 0;
+            }
+
+            currCapacity += weight;
+        }
+        return totalDays;
     }
 }
