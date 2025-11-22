4e3ba202f07ce01a4afd7b824f30803fb7f42f89
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2025-11-22 10:27:32 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (19.97%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,19 +1,18 @@
 class Solution {
     public int search(int[] nums, int target) {
-        return binarySearch(nums, 0, nums.length-1, target);
-    }
+        
+        int left = 0, right = nums.length-1;
 
-    public static int binarySearch(int[] nums, int start, int end, int target){
-        if(start > end)
-            return -1;
+        while(left <= right){
+            int mid = left + (right-left)/2;
 
-        int mid = (start+end)/2;
-
-        if(nums[mid] == target)
-            return mid;
-        else if(nums[mid] < target)
-            return binarySearch(nums, mid+1, end, target);
-        else 
-            return binarySearch(nums, start, mid-1, target);
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] < target)
+                left = mid+1;
+            else
+                right=mid-1;
+        }
+        return -1;
     }
 }
