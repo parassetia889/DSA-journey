8b481a2ce994b87ab7dca9ceaf92688f5089ac4d
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2025-11-22 08:51:37 +0530

    Time: 798 ms (5.49%) | Memory: 47.4 MB (5.16%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,16 +1,18 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        
         if(s1.length() > s2.length())
             return false;
 
         s1 = sort(s1);
 
         for(int i = 0; i <= s2.length()-s1.length(); i++){
-            if(s1.equals(sort(s2.substring(i, i+s1.length())))){
+            String substr = s2.substring(i, i+s1.length());
+
+            if(sort(substr).equals(s1)){
                 return true;
             }
         }
-
         return false;
     }
 
