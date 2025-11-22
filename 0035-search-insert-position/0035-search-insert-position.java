bbe33b7532a4f6b06aee1bb7886a6c90c5234d8a
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2025-11-22 10:36:03 +0530

    Time: 0 ms (100%), Space: 44.7 MB (18.03%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,12 +1,15 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int ans = nums.length;
+        int left = 0, right = nums.length-1;
 
-        for(int i = 0 ; i < nums.length; i++){
-            if(nums[i] >= target)
-                return  i;
+        while(left <= right){
+            int mid = left + (right-left)/2;
+
+            if(nums[mid] >= target)
+                right = mid-1;  
+            else
+                left = mid+1;
         }
-
-        return ans;
+        return left;
     }
 }
