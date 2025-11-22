a127038cd74d3b85c7e16c7385b4ad9ee40e44ae
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2025-11-22 09:58:58 +0530

    Time: 5 ms (56.01%), Space: 47.7 MB (20.29%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,18 +1,45 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         
-        List<Integer> list = new ArrayList<>();
-        for(int i : arr){
-            list.add(i);
+        int right = binarySearch(arr, x);
+        int left = right-1;
+        int n = arr.length;
+
+        while(k > 0){
+            if(left < 0)
+                right++;
+            else if(right >= n )
+                left--;
+            else{
+                if(Math.abs(arr[left]-x) <= Math.abs(arr[right]-x))
+                    left--;
+                else
+                    right++;
+            }
+
+            k--;
         }
 
-        list.sort((a,b) -> {int diff = Math.abs(a-x) - Math.abs(b-x);
-            return diff==0 ? Integer.compare(a,b) : diff;
-            }
-        );
+        List<Integer> res = new ArrayList<>();
+        for(int i = left+1; i < right; i++){
+            res.add(arr[i]);
+        }
 
-        List<Integer> res = list.subList(0,k);
-        Collections.sort(res);
         return res;
+    }
+
+    public int binarySearch(int[] arr, int x){
+        int left = 0, right = arr.length-1;
+
+        while(left <= right){
+            int mid = left + (right-left)/2;
+
+            if(arr[mid] >= x)
+                right = mid-1;
+            else
+                left = mid+1;
+        }
+
+        return left;
     }
 }
