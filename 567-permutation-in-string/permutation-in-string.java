7b727e44881c36052d9f9d7a9f52cd28021e652d
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2025-11-22 09:13:50 +0530

    Time: 5 ms (96.56%) | Memory: 43.7 MB (54.74%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -4,21 +4,38 @@ class Solution {
         if(s1.length() > s2.length())
             return false;
 
-        s1 = sort(s1);
+        int[] freqS1 = new int[26];
+        int[] freqS2 = new int[26];
 
-        for(int i = 0; i <= s2.length()-s1.length(); i++){
-            String substr = s2.substring(i, i+s1.length());
+        for(char ch : s1.toCharArray()){
+            freqS1[ch-'a']++;
+        }
 
-            if(sort(substr).equals(s1)){
-                return true;
-            }
+        int window = s1.length();
+        for(int i = 0; i < window; i++){
+            freqS2[s2.charAt(i)-'a']++;
+        }
+
+        if(matches(freqS1, freqS2))
+            return true;
+
+        for(int i = window; i < s2.length(); i++){
+            freqS2[s2.charAt(i)-'a']++;
+            freqS2[s2.charAt(i-window)-'a']--;
+
+            if(matches(freqS1, freqS2))
+            return true;
         }
         return false;
     }
 
-    public String sort(String str){
-        char[] strArr = str.toCharArray();
-        Arrays.sort(strArr);
-        return new String(strArr);
+    public boolean matches(int[] arr1, int[] arr2){
+
+        for(int i=0; i < 26; i++){
+            if(arr1[i] != arr2[i])
+                return false;
+        }
+
+        return true;
     }
 }
