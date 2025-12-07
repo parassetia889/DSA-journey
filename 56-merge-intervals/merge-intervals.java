87095faf1cb94af6300bd351c5a7a74edee87314
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2025-12-08 00:20:40 +0530

    Time: 8 ms (87.82%) | Memory: 49.1 MB (19.21%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,18 +1,19 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        
-        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
-        int n = intervals.length;
-        int i = 1;
+
+        Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
+
         List<int[]> res = new ArrayList<>();
         res.add(intervals[0]);
+        int i = 1, n = intervals.length;
 
         while(i < n){
-            int interval[] = res.get(res.size()-1);
+            int[] interval = res.get(res.size()-1);
 
             if(interval[1] >= intervals[i][0]){
                 int start = Math.min(interval[0], intervals[i][0]);
                 int end = Math.max(interval[1], intervals[i][1]);
+
                 res.add(new int[]{start, end});
                 res.remove(interval);
             }else{
@@ -20,6 +21,7 @@ class Solution {
             }
             i++;
         }
+
         return res.toArray(new int[res.size()][]);
     }
 }
