894b9bfd62698a72f487070bc18109727b60433e
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2025-12-07 12:28:07 +0530

    Time: 7 ms (6.67%), Space: 46.7 MB (37.87%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        
+        HashSet<ListNode> seen = new HashSet<>();
+        ListNode curr = head;
+
+        while(curr != null){
+            if(seen.contains(curr)){
+                return true;
+            }
+
+            seen.add(curr);
+            curr = curr.next;
+        }
+
+        return false;
+    }
+}
