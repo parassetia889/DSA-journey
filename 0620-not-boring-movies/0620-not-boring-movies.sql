ba504a1985bc1d367fb0caab0a5278bce88a82c4
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2025-12-12 03:33:00 +0530

    Time: 228 ms (59.86%), Space: 0B (100%) - LeetHub

---
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT
+    id, movie, description, rating
+FROM 
+    Cinema
+WHERE
+    description != "boring"
+AND
+    id%2 != 0
+ORDER BY
+    rating desc
