237b0b02bd1b698ad77b9d34d0f5aa0589590a7a
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2025-12-14 17:54:33 +0530

    Time: 5 ms (3.01%) | Memory: 46.7 MB (6.02%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -2,26 +2,28 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
 
         List<List<Integer>> res = new ArrayList<>();
-        boolean[] pick = new boolean[nums.length];
 
-        backTrack(nums, new ArrayList<>(), pick, res);
+        backTrack(nums, 0, res);
         return res;
     }
 
-    public void backTrack(int[] nums, List<Integer> curr, boolean[] pick, List<List<Integer>> res){
-        if(curr.size() == nums.length){
-            res.add(new ArrayList<>(curr));
+    public void backTrack(int[] nums, int indx, List<List<Integer>> res){
+        if(indx >= nums.length){
+            res.add(Arrays.stream(nums).boxed().toList());
             return;
         }
 
-        for(int i=0; i<nums.length; i++){
-            if(!pick[i]){
-                pick[i] = true;
-                curr.add(nums[i]);
-                backTrack(nums, curr, pick, res);
-                curr.remove(curr.size()-1);
-                pick[i]=false;
-            }
+        for(int i=indx; i<nums.length; i++){
+            swap(nums, i, indx);
+            backTrack(nums, indx+1, res);
+            swap(nums, indx, i);
+                
         }
+    }
+
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
