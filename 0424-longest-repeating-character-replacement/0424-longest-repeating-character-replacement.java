7fc029b82d835715d4fcf1ab9f6be845a95ef606
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2025-12-20 10:57:36 +0530

    Time: 7 ms (87.49%), Space: 46.2 MB (50.2%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -2,20 +2,20 @@ class Solution {
     public int characterReplacement(String s, int k) {
         
         int[] count = new int[26];
-        int left = 0, maxLen = 0, maxCount=0;
+        int maxLen = 0, maxCount=0, left=0;
 
         for(int right=0; right < s.length(); right++){
             count[s.charAt(right)-'A']++;
 
             maxCount = Math.max(maxCount, count[s.charAt(right)-'A']);
-
-            while(right-left+1 - maxCount > k){
+            while(right-left-maxCount+1 > k){
                 count[s.charAt(left)-'A']--;
                 left++;
             }
 
             maxLen = Math.max(maxLen, right-left+1);
         }
+
         return maxLen;
     }
 }
