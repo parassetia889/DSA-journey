d81d4509eaa799307aaf928cdd3538a2e0059d52
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2025-12-28 11:18:06 +0530

    Time: 0 ms (100%), Space: 43.9 MB (14.24%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -10,16 +10,14 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode fast = head, slow = head;
-
+        
+        ListNode slow = head, fast = head;
         while(n-- > 0){
             fast = fast.next;
         }
 
-        if(fast == null){
-           
+        if(fast == null)
             return slow.next;
-        }
 
         while(fast.next != null){
             slow = slow.next;
@@ -28,5 +26,6 @@ class Solution {
 
         slow.next = slow.next.next;
         return head;
+
     }
 }
