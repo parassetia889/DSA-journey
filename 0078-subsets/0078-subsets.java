a8d107e01c5de0d6daa3835b7b191d5f2042590c
Paras Setia <59231061+parassetia889@users.noreply.github.com>
2025-12-30 11:45:28 +0530

    Time: 1 ms (86.88%), Space: 43.8 MB (91.17%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -2,21 +2,20 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         
         List<List<Integer>> res = new ArrayList<>();
-
-        generateSubsets(nums, res, 0, new ArrayList<>());
+        generateSubsets(nums, 0, res, new ArrayList<>());
         return res;
     }
 
-    public void generateSubsets(int[] nums, List<List<Integer>> res, int indx, List<Integer> subset){
-        if(indx == nums.length){
+    public void generateSubsets(int[] nums, int indx, List<List<Integer>> res, List<Integer> subset){
+        if(indx >= nums.length){
             res.add(new ArrayList<>(subset));
             return;
         }
 
         subset.add(nums[indx]);
-        generateSubsets(nums, res, indx+1, subset);
-        
+        generateSubsets(nums, indx+1, res, subset);
+
         subset.remove(subset.size()-1);
-        generateSubsets(nums, res, indx+1, subset);
+        generateSubsets(nums, indx+1, res, subset);
     }
 }
